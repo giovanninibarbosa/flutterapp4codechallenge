3e1ed371725ce896855324465e736458da4fc44a
Giovannini Barbosa <giovanninibaarbosa@gmail.com>
2023-12-30 05:24:15 -0300

    refactor: perfom changes to prevent convert 'key' to a super parameter lint warning.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({required this.title, Key? key}) : super(key: key);
+  const HomePage({required this.title, super.key});
 
   final String title;
 
